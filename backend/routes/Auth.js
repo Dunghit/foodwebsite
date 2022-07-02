6838db2257de24fab2d7626dad739a3b41ca9654
Dunghit <dungdell1998@gmail.com>
2022-07-02 12:30:57 +0700

    first commit

---
+++ backend/routes/Auth.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const AuthController = require("../controllers/AuthController");
+router.post("/register", AuthController.registerUser);
+router.post("/login", AuthController.loginUser);
+module.exports = router;
